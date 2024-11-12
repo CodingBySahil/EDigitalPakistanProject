4ed74dc25980867769cf406afe5e1fd9ae72719a
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-11-12 17:19:34 +0500

    payment branch created

--- src/components/payment/Payment.jsx
+++ src/components/payment/Payment.jsx
@@ -7,7 +7,7 @@ export default function Payment() {
   // JSX
   return (
     <div className="mb-[0px] ml-[0px] mr-[0px] mt-[0px] w-[100] p-[0] text-[10px]">
-      payment
+      payment branch created
     </div>
   );
   // JSX
