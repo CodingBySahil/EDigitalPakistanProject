acee67d49686765a668e144501435b7d5b766c54
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-11-13 18:20:26 +0500

    payment form working correctly

--- src/components/FormTextField.jsx
+++ src/components/FormTextField.jsx
@@ -1,7 +1,12 @@
 import PropTypes from "prop-types";
 
 // COMPONENT START
-export default function FormTextField({ placeholder, id }) {
+export default function FormTextField({
+  validationObj = {},
+  register,
+  placeholder,
+  id,
+}) {
   // VARIABLES
 
   // FUNCTIONS
@@ -10,6 +15,7 @@ export default function FormTextField({ placeholder, id }) {
   return (
     <input
       id={id}
+      {...register(`${id}`, { ...validationObj })}
       type="text"
       placeholder={placeholder}
       className="mt-[5px] h-[35px] w-full rounded-[5px] border border-solid border-slate-200 pl-[10px] text-[14px] focus:outline-none focus:ring-[1px] focus:ring-brand-color-cyan"
@@ -21,6 +27,8 @@ export default function FormTextField({ placeholder, id }) {
 FormTextField.propTypes = {
   placeholder: PropTypes.string,
   id: PropTypes.string,
+  register: PropTypes.func,
+  validationObj: PropTypes.object,
 };
 //size: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 // COMPONENT END
