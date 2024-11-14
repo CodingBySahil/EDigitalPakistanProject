f49bdbb8e4cc72f85aaa860f30c57c9e81081dcf
codingBySahil <muhammadsahil345737@gmail.com>
2024-11-14 15:28:11 +0500

    explore courses styled managed

--- src/components/ExploreCourses.jsx
+++ src/components/ExploreCourses.jsx
@@ -1,9 +1,9 @@
 import { PropTypes } from "prop-types";
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const ExploreCourses = ({ levelOfClass }) => {
-
   const getBackgroundColor = (subject) => {
     switch (subject) {
       case "English":
@@ -27,16 +27,16 @@ const ExploreCourses = ({ levelOfClass }) => {
 
   return (
     <>
-      <div className="overflow-hidden bg-[#ebf5ff] py-8">
+      <div className="overflow-y-hidden bg-[#ebf5ff] py-8">
         {/* Class Title */}
         <div className="text-lg mb-6 w-fit rounded-md bg-red-600 px-6 py-2 text-white shadow-md">
           Class {levelOfClass}th
         </div>
 
         {/* Container */}
-        <div className="flex justify-between">
+        <div className=" ">
           {/* Subject Cards Container */}
-          <div className="relative grid grid-cols-4 gap-4 md:grid-cols-9 lg:grid-cols-12">
+          <div className="relative flex flex-wrap gap-2">
             {[
               "English",
               "General Science",
@@ -49,21 +49,19 @@ const ExploreCourses = ({ levelOfClass }) => {
               const [isHovered, setIsHovered] = useState(false);
 
               return (
-                <div key={index} className="flex hover:min-w-[50rem]">
+                <div key={index} className="flex">
                   <div
                     className="flex items-center justify-center"
                     onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}
                   >
                     <div
-                      className={`flex h-[20rem]  -rotate-[8deg] transform rounded-lg bg-white p-2 shadow-lg transition-all duration-300 ease-in-out ${
-                        isHovered ? "z-10 scale-110" : ""
-                      }`}
+                      className={`flex h-[20rem] -rotate-[8deg] transform rounded-lg bg-white p-2 shadow-lg transition-all duration-300 ease-in-out ${isHovered ? "z-10 scale-105" : ""}`}
                     >
                       <div className="h-full max-w-20 rounded-lg bg-green-200 p-2 shadow-md">
                         <div
                           className={`flex h-full flex-shrink-0 items-start justify-center rounded-md px-4 py-2 text-center text-white ${getBackgroundColor(
-                            subject
+                            subject,
                           )}`}
                         >
                           <p className="my-auto w-auto rotate-[90deg] transform whitespace-nowrap text-xl">
@@ -74,7 +72,7 @@ const ExploreCourses = ({ levelOfClass }) => {
                       <div>
                         {/* Hovered Subject Book Design */}
                         {isHovered && (
-                          <div className="hover:scale-103 max-h-full w-96 transform p-2 transition-all duration-300 ease-in-out hover:shadow-2xl md:p-4">
+                          <div className="hover:scale-103 w-96 transform p-2 transition-all duration-300 ease-in-out hover:shadow-2xl md:p-4">
                             <div className="animate-fadeIn flex h-72 rounded-lg bg-white p-4 opacity-0 shadow-lg">
                               <img
                                 src="/book-1@2x.png" // respective image path
@@ -86,20 +84,25 @@ const ExploreCourses = ({ levelOfClass }) => {
                                   {subject} - Class {levelOfClass}th
                                 </div>
                                 <div className="md:text-sm text-xs text-gray-600">
-                                  Course textbooks, include all subjects taught in
-                                  Govt. schools.
+                                  Course textbooks, include all subjects taught
+                                  in Govt. schools.
                                 </div>
                                 <div className="md:text-lg mt-4 text-base font-bold">
                                   Rs. 1500
                                 </div>
                                 <div className="flex items-center gap-0.5">
                                   {[...Array(5)].map((_, i) => (
-                                    <FaStar key={i} className="h-5 w-5 text-yellow-300 hover:text-yellow-500" />
+                                    <FaStar
+                                      key={i}
+                                      className="h-5 w-5 text-yellow-300 hover:text-yellow-500"
+                                    />
                                   ))}
                                 </div>
-                                <button className="md:text-sm mt-2 rounded-md bg-blue-400 px-4 py-2 text-xs text-white transition-colors hover:bg-blue-600 lg:text-base cursor-pointer">
-                                  Explore
-                                </button>
+                                <Link to={'/course-calender'}>
+                                  <button className="md:text-sm mt-2 cursor-pointer rounded-md bg-blue-400 px-4 py-2 text-xs text-white transition-colors hover:bg-blue-600 lg:text-base">
+                                    Explore
+                                  </button>
+                                </Link>
                               </div>
 
                               <button
