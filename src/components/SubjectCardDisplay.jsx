e7aa55824295a2f89786afacc81ea9e58769d8ab
codingBySahil <muhammadsahil345737@gmail.com>
2024-11-28 13:00:19 +0500

    props validation added for subjectcard array

--- src/components/SubjectCardDisplay.jsx
+++ src/components/SubjectCardDisplay.jsx
@@ -1,39 +1,37 @@
+import { PropTypes } from "prop-types";
 import { Link } from "react-router-dom";
 import { brandColorCyan } from "../constants/brandColors";
 
 // COMPONENT START
 export default function SubjectCardDisplay({ cardsArr }) {
-  
-
-
   return (
-    <section className="grid grid-cols-1 tabS:grid-cols-2 laptop14:grid-cols-4 gap-8 laptop14:gap-[60px]">
+    <section className="grid grid-cols-1 gap-8 tabS:grid-cols-2 laptop14:grid-cols-4 laptop14:gap-[60px]">
       {cardsArr.map((val, index) => (
         <div
           id={`subjectCard${index}`}
           key={index}
           style={{ border: `1px solid ${brandColorCyan}` }}
-          className="min-h-[400px] bg-brand-color-cyan/40 p-[8px] grid grid-rows-[300px_1fr_50px] rounded-[8px]"
+          className="grid min-h-[400px] grid-rows-[300px_1fr_50px] rounded-[8px] bg-brand-color-cyan/40 p-[8px]"
         >
           {/* Image Section */}
-          <section className="bg-lightsteelblue-200 rounded-[5px]">
+          <section className="rounded-[5px] bg-lightsteelblue-200">
             <img
               src={`book-1@2x.png`}
               alt={val?.title}
-              className="w-full h-full object-cover object-top rounded-[5px]"
+              className="h-full w-full rounded-[5px] object-cover object-top"
             />
           </section>
 
           {/* Title Section */}
-          <section className="min-h-[50px] mt-[5px] min-w-[200px]">
-            <p className="text-[17px] font-bold break-words">{val?.title}</p>
+          <section className="mt-[5px] min-h-[50px] min-w-[200px]">
+            <p className="break-words text-[17px] font-bold">{val?.title}</p>
           </section>
 
           {/* Button Section */}
           <section className="flex items-center justify-end">
-            <button className="px-[10px] py-[10px] cursor-pointer rounded-[5px] bg-brand-color-cyan active:bg-brand-color-cyan/80">
+            <button className="cursor-pointer rounded-[5px] bg-brand-color-cyan px-[10px] py-[10px] active:bg-brand-color-cyan/80">
               <Link
-                className="text-[16px] no-underline text-white"
+                className="text-[16px] text-white no-underline"
                 to={`/administration/${val?.code}`}
               >
                 Upload content
@@ -45,4 +43,9 @@ export default function SubjectCardDisplay({ cardsArr }) {
     </section>
   );
 }
+
+// adding props validation for cardsArr
+SubjectCardDisplay.propTypes = {
+  cardsArr: PropTypes.array.isRequired,
+};
 // COMPONENT END
