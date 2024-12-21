3a3aef9c9806450e79c0e5036816f7b201df0fcb
codingBySahil <muhammadsahil345737@gmail.com>
2024-12-21 22:17:56 +0500

    change mainURL in constants

--- src/constants/const.js
+++ src/constants/const.js
@@ -1,3 +1,5 @@
-export const mainURL = "https://new-lms.onrender.com";
+// export const mainURL = "https://new-lms.onrender.com";
+export const mainURL = "https://lms.digipakistan.com/";
+
 export const sideBarToggleFalse992 = 992;
 export const stripePaymentURL = "http://127.0.0.1:3000/payment-intent";
