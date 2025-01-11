d7f3435df6fd528ea02e4e1efeb6dad973c6738d
codingBySahil <muhammadsahil345737@gmail.com>
2025-01-11 11:52:04 +0500

    class input auto fill from url

--- src/components/SubjectCardDisplay.jsx
+++ src/components/SubjectCardDisplay.jsx
@@ -3,6 +3,9 @@ import SubjectCard from "./SubjectCard";
 import { mainURL } from "../constants/const";
 
 export default function SubjectCardDisplay({ cardsArr }) {
+  // reading class number from url
+  const urlParams = new URLSearchParams(window.location.search);
+  const classNumber = urlParams.get("class");
   return (
     <section className="grid grid-cols-1 gap-8 tabS:grid-cols-2 laptop14:grid-cols-4 laptop14:gap-[60px]">
       {cardsArr.map((val, index) => (
@@ -10,7 +13,7 @@ export default function SubjectCardDisplay({ cardsArr }) {
           key={index}
           subjectName={val?.title}
           buttonText={"Upload content"}
-          buttonUrl={`/administration/${val?.code}`}
+          buttonUrl={`/administration/${val?.code}?class=${classNumber}`}
           imgPath={`/${val?.subPic}`}
         />
       ))}
